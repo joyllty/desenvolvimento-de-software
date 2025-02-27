85c16bfa150c0e96b7791ea3080b73d4ce5146bd
bruna chapelin <161771692+joyllty@users.noreply.github.com>
2025-02-27 10:50:29 -0300

    Create ex01.java

---
+++ ex01.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//Peça para o usuário digitar um número e exiba esse número multiplicado por 2
+    Scanner leitor = new Scanner(System.in);
+
+    System.out.println("Bem vindo(a)!");
+    System.out.println("Digite um número: ");
+    int num  = leitor.nextInt();
+
+    System.out.println("O dobro do número é: " + num * 2);
+
+    leitor.close();
+    }
+}

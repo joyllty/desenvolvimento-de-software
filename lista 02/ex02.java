efadb6eec0b805e7a1aa34edf62614087dc294d5
bruna chapelin <161771692+joyllty@users.noreply.github.com>
2025-03-13 18:03:36 -0300

    Update ex02.java

--- lista 02/ex02.java
+++ lista 02/ex02.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 /* Escreva um algoritmo que leia em um vetor uma sequência finita de números digitados pelo usuário e, logo após,
 mostre o número de cada posição do vetor e se ele é positivo, negativo ou zero. */
-public class ex01 {
+public class ex02 {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);

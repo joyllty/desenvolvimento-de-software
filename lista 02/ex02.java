92f0d6d9b3b298c984cdd8d68531f3e583394ed3
bruna chapelin <161771692+joyllty@users.noreply.github.com>
2025-03-13 09:38:02 -0300

    Create ex02.java

---
+++ lista 02/ex02.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+/* Escreva um algoritmo que leia em um vetor uma sequência finita de números digitados pelo usuário e, logo após,
+mostre o número de cada posição do vetor e se ele é positivo, negativo ou zero. */
+public class ex01 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int i, tam_Vetor;
+
+        System.out.println("Quantos valores você irá digitar?: ");
+        tam_Vetor = input.nextInt();
+
+        int[] vetor = new int[tam_Vetor];
+
+        System.out.printf("Digite apenas valores inteiros!");
+
+        /* Lendo os valores */
+        for (i = 0; i < vetor.length; i++) {
+            System.out.printf("\nValor [%d]: ", i + 1);
+            vetor[i] = input.nextInt();
+        }
+        /* Apresentando os valores */
+        for (i = 0; i < vetor.length; i++) {
+            /* Verificação se o número é positivo, negativo ou zero */
+            if (vetor[i] > 0) {
+                System.out.printf("\nValor [%d]: %d é positivo!", i + 1, vetor[i]);
+            } else if (vetor[i] == 0) {
+                System.out.printf("\nValor [%d]: %d é igual a zero!", i + 1, vetor[i]);
+            } else {
+                System.out.printf("\nValor [%d]: %d é negativo!", i + 1, vetor[i]);
+            }
+        }
+    }
+}

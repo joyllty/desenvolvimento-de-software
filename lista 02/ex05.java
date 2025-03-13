e4d128e9e9ca77902da35acc63fc2df3098c9507
bruna chapelin <161771692+joyllty@users.noreply.github.com>
2025-03-13 18:03:12 -0300

    Update ex05.java

--- lista 02/ex05.java
+++ lista 02/ex05.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 /* Elabore um programa que leia 5 números do teclado e os armazene em um vetor. Em seguida, leia um número n
 qualquer e apresente na tela a quantidade de vezes que o número n aparece no vetor. */
-public class ex01 {
+public class ex05 {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);

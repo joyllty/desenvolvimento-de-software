bad1758b98490104c95438557e8bf077d9fdc9b6
Bruna Chapelin <161771692+joyllty@users.noreply.github.com>
2025-04-03 11:09:34 -0300

    Create Personagem.java

---
+++ joguinho(estudar)/Personagem.java
@@ -0,0 +1,13 @@
+public class Personagem {
+    int posX; //linha
+    int posY; //coluna
+    String desenho; // 💀
+
+    Personagem(int x, int y, String des){
+        this.posX = x;
+        this.posY = y;
+        this.desenho = des;
+    }
+
+
+}

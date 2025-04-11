af6b258aa36b60a08c3b3db5fc848a50cd701424
Bruna Chapelin <161771692+joyllty@users.noreply.github.com>
2025-04-11 11:24:40 -0300

    Update Main.java

--- aulas/aula14/Main.java
+++ aulas/aula14/Main.java
@@ -6,7 +6,32 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        List<Personagem> personagens = new ArrayList<Personagem>();
+        Personagem[] personagens = new Personagem[3];
+
+        personagens[0] = new Arqueiro("Thebit", 50, 20);
+        personagens[1] = new Guerreiro("Rell", 70, 35);
+        personagens[2] = new Mago("Soraka", 50, 25);
+
+        for(int i = 0; i < 3; i++){
+            personagens[i].atacar();
+        }
+
+        System.out.println("\n==================");
+        System.out.println("\n[1] Atacar\n[2]Exibir personagens\n[3]Sair");
+        System.out.println("\n==================");
+        System.out.println("\n Escolha uma opção: ");
+        int op = input.nextInt();
+        input.nextLine();
+
+        switch(op){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+        }
 
 
     }

3d92bb13c4789dbb48b34a453985eb6544d529c5
Bruna Chapelin <161771692+joyllty@users.noreply.github.com>
2025-04-11 11:25:43 -0300

    Update Personagem.java

--- aulas/aula14/Personagem.java
+++ aulas/aula14/Personagem.java
@@ -1,18 +1,44 @@
 public class Personagem {
     private String nome;
-    private String tipo;
-    private String ataque;
+    private int vida;
+    private int dano;
 
-    public Personagem(){
-
+    public Personagem(String nome, int vida, int dano){
+        this.nome = nome;
+        this.vida = vida;
+        this.dano = dano;
+    }
+    public void atacar() {
+        System.out.printf("%s atacou!\n", getNome());
     }
 
-    public void atacar(){
-        System.out.printf("%s atacou!", getNome());
+    public int receberDano(){
+        this.vida -= this.dano;
+        return vida;
+    }
+    public boolean estaVivo(){
+        boolean vivo;
+        if(receberDano() < 0){
+            System.out.printf("\n%s MORREU", getNome());
+            return vivo = false;
+        } else {
+            return vivo = true;
+        }
     }
 
+    public String getInfo(){
+        return "> Nome: " + nome + "\n" + "> Vida: " + vida + "> Dano: " + dano;
+    }
     public String getNome(){
         return nome;
     }
+    public int getVida(){
+        return vida;
+    }
+    public int getDano(){
+        return dano;
+    }
+
+
 
 }

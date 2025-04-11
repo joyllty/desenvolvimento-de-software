7df10fcf166d370f4da16574e58c14738f87ce35
Bruna Chapelin <161771692+joyllty@users.noreply.github.com>
2025-04-11 11:25:54 -0300

    Update Arqueiro.java

--- aulas/aula14/Arqueiro.java
+++ aulas/aula14/Arqueiro.java
@@ -1,7 +1,11 @@
 public class Arqueiro extends Personagem{
 
+    public Arqueiro(String nome, int vida, int dano){
+        super(nome, vida, dano);
+    }
     @Override
     public void atacar(){
-        System.out.printf("%s disparou uma flecha encantada pelos elfos!", getNome());
+        System.out.printf("%s disparou uma flecha encantada pelos elfos!\n", getNome());
+
     }
 }

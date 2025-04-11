4e14171e880a62d93b0104daa67644557017b86a
Bruna Chapelin <161771692+joyllty@users.noreply.github.com>
2025-04-11 11:26:02 -0300

    Update Guerreiro.java

--- aulas/aula14/Guerreiro.java
+++ aulas/aula14/Guerreiro.java
@@ -1,7 +1,11 @@
 public class Guerreiro extends Personagem{
 
+    public Guerreiro (String nome, int vida, int dano){
+        super(nome, vida, dano);
+    }
+
     @Override
     public void atacar(){
-        System.out.printf("%s atacou com sua espada muito afiada!", getNome());
+        System.out.printf("%s atacou com sua espada muito afiada!\n", getNome());
     }
 }

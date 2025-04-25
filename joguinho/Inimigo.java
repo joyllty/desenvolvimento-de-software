8a452c52b287a5b8850a1a0825c0694d60258448
Bruna Chapelin <161771692+joyllty@users.noreply.github.com>
2025-04-25 10:23:56 -0300

    Delete joguinho directory

--- joguinho/Inimigo.java
+++
@@ -1,9 +0,0 @@
-public class Inimigo extends Personagem{
-    int posX, posY;
-    String desenho;
-
-    Inimigo(int x, int y, String des){
-        super(x, y, des); //passa as informações para a classe personagem
-                            // sempre vai chamar o construtor da classe de cima
-    }
-}
--- joguinho/Player.java
+++
@@ -1,9 +0,0 @@
-public class Player extends Personagem {
-    int posX, posY;
-    String desenho;
-
-    Player(int x, int y, String des){
-        super(x, y, des);
-    }
-
-}

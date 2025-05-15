ce515ca19e47fe065353283ca581980594ec93e9
Bruna Chapelin <161771692+joyllty@users.noreply.github.com>
2025-05-15 09:59:52 -0300

    Create Calculadora.java

---
+++ herança e polimorfismo/sobrecarga de metodos(overload)/Calculadora.java
@@ -0,0 +1,18 @@
+public class Calculadora {
+
+    public static int soma (int n1, int n2){
+        return (n1 + n2);
+    }
+
+    public static float soma (float n1, float n2){
+        return (n1 + n2);
+    }
+
+    public static float soma (float n1, float n2, float n3){
+        return (n1 + n2 + n3);
+    }
+
+    public static double soma (double n1, double n2, double n3, double n4){
+        return (n1 + n2 + n3 + n4);
+    }
+}

7e201d211778eabb1a7f5bd52714c5481d5354ac
Bruna Chapelin <161771692+joyllty@users.noreply.github.com>
2025-05-15 09:59:11 -0300

    Create  Main.java

---
+++ herança e polimorfismo/sobrecarga de metodos(overload)/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int soma1 = Calculadora.soma(2,2);
+
+        float soma2 = Calculadora.soma(2.5f, 3.2f);
+
+        double soma3 = Calculadora.soma(2, 4.5, 6, 7.8);
+
+
+        Calculadora.soma(3, 5);
+
+        Calculadora.soma(4.5f, 1.3f, 2);
+
+        Calculadora.soma(2.6, 7.8, 1.5, 6.2);
+
+    }
+}

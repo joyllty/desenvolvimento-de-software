326774757ce6c857015ff763bdbb71c7b6414a4e
Bruna Chapelin <161771692+joyllty@users.noreply.github.com>
2025-05-22 10:33:34 -0300

    Create Main.java

---
+++ revisao projeto/View/Main.java
@@ -0,0 +1,20 @@
+package View;
+import Model.Aluno;
+import Model.Pessoa;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Sistema.executar();
+
+        Aluno a = new Aluno ("joao", "j@j.com", 1234567);
+
+        //se tiver fora do pacote nao da pra acessar
+        a.setMatricula(2);
+
+    }
+}

d1e9de4d2696c6c9c5a39598f7ed88bc8b169c3b
Bruna Chapelin <161771692+joyllty@users.noreply.github.com>
2025-05-22 10:31:50 -0300

    Create CadastroAlunos.java

---
+++ revisao projeto/Controller/CadastroAlunos.java
@@ -0,0 +1,25 @@
+package Controller;
+
+import Model.Aluno;
+import java.util.ArrayList;
+
+public class CadastroAlunos {
+
+    private static ArrayList<Aluno> listaAlunos = new ArrayList<Aluno>();
+
+    public static void cadastrarAluno(String nome, String email, int mat){
+        Aluno novoAluno = new Aluno(nome, email, mat);
+        listaAlunos.add(novoAluno);
+    }
+    public static void cadastrarAluno(Aluno novoAluno){
+        listaAlunos.add(novoAluno);
+    }
+
+    public static ArrayList<Aluno> listaDeAlunos(){
+        return listaAlunos;
+    }
+
+    public static boolean listaVazia(){
+        return listaAlunos.isEmpty();
+    }
+}
